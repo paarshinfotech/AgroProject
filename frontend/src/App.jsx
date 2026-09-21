029e83adca8c0a86dad46eee674be798941cbb6b
Sujal Raut <sujalbraut@gmail.com>
2026-09-21 18:39:40 +0530

    updation of app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import Sidebar from "./Admin/Components/Sidebar";
+import Dashboard from "./Admin/Pages/Dashboards";
 import CustomerManagement from "./Admin/Pages/CustomerManagement";
+import Report from "./Admin/Pages/Report";
 
 function App() {
   const [currentPage, setCurrentPage] = useState("Dashboard");
@@ -9,9 +11,21 @@ function App() {
     <>
       <Sidebar setCurrentPage={setCurrentPage} />
 
+      {currentPage === "Dashboard" && (
+        <div style={{ marginLeft: "260px" }}>
+          <Dashboard />
+        </div>
+      )}
+
       {currentPage === "Customer Management" && (
         <div style={{ marginLeft: "260px" }}>
           <CustomerManagement />
+        </div>
+      )}
+
+      {currentPage === "Reports" && (
+        <div style={{ marginLeft: "260px" }}>
+          <Report />
         </div>
       )}
     </>
